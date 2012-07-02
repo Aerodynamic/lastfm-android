764d034d5572a6100b8b643d14fbb714a8d644ad
Pablo Alcayaga <pablo.alcayaga@gmail.com>
2012-07-01 21:37:26 -0400

    Support for Offline Scrobbles

--- app/src/fm/last/android/scrobbler/ScrobblerService.java
+++ app/src/fm/last/android/scrobbler/ScrobblerService.java
@@ -820,6 +820,11 @@ public class ScrobblerService extends Service {
 					success = true; //Remove the track from the queue
 				}
 				if(success) {
+					ScrobblerQueueEntry[] array = {e};
+					
+					LocalScrobbler ls = new LocalScrobbler();
+					ls.Scrobble(array);					
+					
 					ScrobblerQueueDao.getInstance().removeFromQueue(e);
 				} 
 				else {
